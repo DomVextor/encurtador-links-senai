ed2fc0c876fade26b0b655250093f45f345ed1e0
DomVextor <domvextor@github.com>
2026-05-09 15:33:25 -0300

    refactor: usar variaveis de ambiente para configuracao do firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmzkHGCx3vf6FPZUNMdMphGAnDqb6JPjQ",
-  authDomain: "encurtador-links-senai.firebaseapp.com",
-  projectId: "encurtador-links-senai",
-  storageBucket: "encurtador-links-senai.firebasestorage.app",
-  messagingSenderId: "910094002894",
-  appId: "1:910094002894:web:c0c6b355e31204283efe9f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
